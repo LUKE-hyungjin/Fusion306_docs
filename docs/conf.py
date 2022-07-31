0ca800e551cecf7102b21fa7b5aca25de12334f7
LUKE-hyungjin <e93052080@gmail.com>
2022-07-31 21:41:36 +0900

    first setting

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,10 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx_book_theme']
+source_suffix={
+    '.rst':'restructuredtext',
+    '.md':'markdown'
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
