62e4e07a37f3711c08aa186e61f1e9fee12669ee
LUKE (HYUNGJIN LEE) <68213792+LUKE-hyungjin@users.noreply.github.com>
2023-03-11 03:13:25 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -83,10 +83,3 @@ html_sidebars = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-# Add Unicode language settings for Korean
-latex_engine = 'xelatex'
-latex_use_xindy = False
-latex_elements = {
-    'preamble': '\\usepackage{kotex}\n',
-}
